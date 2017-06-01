ec745e6a577b609b1705e854c9f96db2ac1b93cf
Cristian Pallarés <cpallares@inbenta.com>
2017-06-01 12:02:43 +0200

    Use templates for the value type

--- triangle.h
+++ triangle.h
@@ -4,29 +4,57 @@
 #include "vector2.h"
 #include "edge.h"
 
+#include <assert.h>
+#include <math.h>
+
+template <class T>
 class Triangle
 {
 	public:
-		Triangle(const Vec2f &_p1, const Vec2f &_p2, const Vec2f &_p3);
+		using EdgeType = Edge<T>;
+		using VertexType = Vector2<T>;
+		
+		Triangle(const VertexType &_p1, const VertexType &_p2, const VertexType &_p3)
+		:	p1(_p1), p2(_p2), p3(_p3),
+			e1(_p1, _p2), e2(_p2, _p3), e3(_p3, _p1)
+		{}
 	
-		bool containsVertex(const Vec2f &v);
-		bool circumCircleContains(const Vec2f &v);
+		bool containsVertex(const VertexType &v)
+		{
+			return p1 == v || p2 == v || p3 == v; 
+		}
+		
+		bool circumCircleContains(const VertexType &v)
+		{
+			float ab = (p1.x * p1.x) + (p1.y * p1.y);
+			float cd = (p2.x * p2.x) + (p2.y * p2.y);
+			float ef = (p3.x * p3.x) + (p3.y * p3.y);
+
+			float circum_x = (ab * (p3.y - p2.y) + cd * (p1.y - p3.y) + ef * (p2.y - p1.y)) / (p1.x * (p3.y - p2.y) + p2.x * (p1.y - p3.y) + p3.x * (p2.y - p1.y)) / 2.f;
+			float circum_y = (ab * (p3.x - p2.x) + cd * (p1.x - p3.x) + ef * (p2.x - p1.x)) / (p1.y * (p3.x - p2.x) + p2.y * (p1.x - p3.x) + p3.y * (p2.x - p1.x)) / 2.f;
+			float circum_radius = sqrtf(((p1.x - circum_x) * (p1.x - circum_x)) + ((p1.y - circum_y) * (p1.y - circum_y)));
+
+			float dist = sqrtf(((v.x - circum_x) * (v.x - circum_x)) + ((v.y - circum_y) * (v.y - circum_y)));
+			return dist <= circum_radius;
+		}
 	
-		Vec2f p1;
-		Vec2f p2;
-		Vec2f p3;
-		Edge e1;				
-		Edge e2;
-		Edge e3;
+		VertexType p1;
+		VertexType p2;
+		VertexType p3;
+		EdgeType e1;				
+		EdgeType e2;
+		EdgeType e3;
 };
 
-inline std::ostream &operator << (std::ostream &str, const Triangle & t)
+template <class T>
+inline std::ostream &operator << (std::ostream &str, const Triangle<T> & t)
 {
 	return str << "Triangle:" << std::endl << "\t" << t.p1 << std::endl << "\t" << t.p2 << std::endl << "\t" << t.p3 << std::endl << "\t" << t.e1 << std::endl << "\t" << t.e2 << std::endl << "\t" << t.e3 << std::endl;
 		
 }
 
-inline bool operator == (const Triangle &t1, const Triangle &t2)
+template <class T>
+inline bool operator == (const Triangle<T> &t1, const Triangle<T> &t2)
 {
 	return	(t1.p1 == t2.p1 || t1.p1 == t2.p2 || t1.p1 == t2.p3) &&
 			(t1.p2 == t2.p1 || t1.p2 == t2.p2 || t1.p2 == t2.p3) && 
