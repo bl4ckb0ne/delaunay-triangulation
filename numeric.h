df7f12046222ae1b263d9148d493182617edc611
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2018-05-08 21:14:30 +0200

    Add reference for almost_equal function

--- numeric.h
+++ numeric.h
@@ -4,6 +4,10 @@
 #include <cmath>
 #include <limits>
 
+/**
+ * @brief use of machine epsilon to compare floating-point values for equality
+ * http://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+ */
 template<class T>
 typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
     almost_equal(T x, T y, int ulp=2)
@@ -12,7 +16,7 @@ typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
     // and multiplied by the desired precision in ULPs (units in the last place)
     return std::abs(x-y) <= std::numeric_limits<T>::epsilon() * std::abs(x+y) * ulp
     // unless the result is subnormal
-           || std::abs(x-y) < std::numeric_limits<T>::min();
+        || std::abs(x-y) < std::numeric_limits<T>::min();
 }
 
 template<class T>
