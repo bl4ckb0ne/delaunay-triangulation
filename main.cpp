6fa64b465fae1d4d2e754c29015f00a9aa853bc3
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2018-05-08 15:41:49 +0200

    Fix alignment in main.cpp

--- main.cpp
+++ main.cpp
@@ -81,15 +81,15 @@ int main(int argc, char * argv[])
 
 	while (window.isOpen())
 	{
-	        sf::Event event;
-	        while (window.pollEvent(event))
-	        {
-	            if (event.type == sf::Event::Closed)
-	                window.close();
-	        }
-	
-	        window.clear();
-	
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
 		// Draw the squares
 		for(auto s = begin(squares); s != end(squares); s++) {
 			window.draw(**s);
