01d980f584ff936cf3d49c4850c29da19eef8f84
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2018-05-09 17:02:58 +0200

    Convert google tests to catch tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,57 +1,30 @@
-#include "gtest/gtest.h"
-
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "delaunay.h"
 
-namespace {
-
-// The fixture for testing class Foo.
-class DelaunayTest : public ::testing::Test {
-    protected:
-    // You can remove any or all of the following functions if its body
-    // is empty.
-    DelaunayTest() {
-        // You can do set-up work for each test here.
-    }
-    virtual ~DelaunayTest() {
-        // You can do clean-up work that doesn't throw exceptions here.
-    }
-    // If the constructor and destructor are not enough for setting up
-    // and cleaning up each test, you can define the following methods:
-    virtual void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
-    }
-    virtual void TearDown() {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-    }
-    // Objects declared here can be used by all tests in the test case for Foo.
-};
-
-
-TEST_F(DelaunayTest, DelaunayFloat) {
+TEST_CASE("Delaunay triangulation should be able to triangulate 3 points as float", "[DelaunayTest]") {
     std::vector<Vector2<float> > points;
     points.push_back(Vector2<float>(0.0f, 0.0f));
     points.push_back(Vector2<float>(1.0f, 0.0f));
     points.push_back(Vector2<float>(0.0f, 1.0f));
     Delaunay<float> triangulation;
     const std::vector<Triangle<float> > triangles = triangulation.triangulate(points);
-    EXPECT_EQ(1, triangles.size());
+    REQUIRE(1 == triangles.size());
     // const std::vector<Edge<float> > edges = triangulation.getEdges();
 }
 
-TEST_F(DelaunayTest, DelaunayDouble) {
+TEST_CASE( "Delaunay triangulation should be able to triangulate 3 points as double", "[DelaunayTest]" ) {
     std::vector<Vector2<double> > points;
     points.push_back(Vector2<double>(0.0, 0.0));
     points.push_back(Vector2<double>(1.0, 0.0));
     points.push_back(Vector2<double>(0.0, 1.0));
     Delaunay<double> triangulation;
     const std::vector<Triangle<double> > triangles = triangulation.triangulate(points);
-    EXPECT_EQ(1, triangles.size());
+    REQUIRE(1 == triangles.size());
     // const std::vector<Edge<double> > edges = triangulation.getEdges();
 }
 
-TEST_F(DelaunayTest, DelaunayDoubleDegenerated) {
+TEST_CASE("Delaunay triangulation should be able to handle duplicated 3 points as double", "[DelaunayTest]") {
     std::vector<Vector2<double> > points;
     points.push_back(Vector2<double>(0.0, 0.0));
     points.push_back(Vector2<double>(0.0, 0.0));
@@ -59,11 +32,11 @@ TEST_F(DelaunayTest, DelaunayDoubleDegenerated) {
     points.push_back(Vector2<double>(0.0, 1.0));
     Delaunay<double> triangulation;
     const std::vector<Triangle<double> > triangles = triangulation.triangulate(points);
-    EXPECT_EQ(1, triangles.size());
+    REQUIRE(1 == triangles.size());
     // const std::vector<Edge<double> > edges = triangulation.getEdges();
 }
 
-TEST_F(DelaunayTest, DelaunayFloat10000) {
+TEST_CASE("Delaunay triangulation should be able to handle 10000 points as float", "[DelaunayTest]") {
     std::vector<Vector2<float> > points(1e4);
     srand(666);
     for (size_t i=0; i < 1e4; ++i)
@@ -72,12 +45,12 @@ TEST_F(DelaunayTest, DelaunayFloat10000) {
         float y = (double)rand() / (float)RAND_MAX;
         points.at(i) = Vector2<float>(x, y);
     }
-    EXPECT_EQ(10000, points.size());
+    REQUIRE(10000 == points.size());
     Delaunay<float> triangulation;
     const std::vector<Triangle<float> > triangles = triangulation.triangulate(points);
 }
 
-TEST_F(DelaunayTest, DelaunayDouble10000) {
+TEST_CASE("Delaunay triangulation should be able to handle 10000 points as double", "[DelaunayTest]") {
     std::vector<Vector2<double> > points(1e4);
     srand(666);
     for (size_t i=0; i < 1e4; ++i)
@@ -86,14 +59,7 @@ TEST_F(DelaunayTest, DelaunayDouble10000) {
         double y = (double)rand() / (double)RAND_MAX;
         points.at(i) = Vector2<double>(x, y);
     }
-    EXPECT_EQ(10000, points.size());
+    REQUIRE(10000 == points.size());
     Delaunay<double> triangulation;
     const std::vector<Triangle<double> > triangles = triangulation.triangulate(points);
-}
-
-}   // namespace
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
 }
