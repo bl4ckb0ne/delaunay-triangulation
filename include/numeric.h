a4ff4f63e0d99bf9b82e6cb14c7e32a5065c4c06
Simon Zeni <simonzeni@gmail.com>
2019-05-24 13:58:57 -0400

    detemplate numeric

--- include/numeric.h
+++ include/numeric.h
@@ -8,24 +8,8 @@
  * @brief use of machine epsilon to compare floating-point values for equality
  * http://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
  */
-template<class T>
-typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
-    almost_equal(T x, T y, int ulp=2)
-{
-    // the machine epsilon has to be scaled to the magnitude of the values used
-    // and multiplied by the desired precision in ULPs (units in the last place)
-    return std::abs(x-y) <= std::numeric_limits<T>::epsilon() * std::abs(x+y) * static_cast<T>(ulp)
-    // unless the result is subnormal
-        || std::abs(x-y) < std::numeric_limits<T>::min();
-}
+bool almost_equal(const double x, const double y, int ulp=2);
 
-template<class T>
-T half(T x){}
-
-template <>
-float half(float x){return 0.5f * x;}
-
-template <>
-double half(double x){return 0.5 * x;}
+double half(const double x);
 
 #endif
