adfe2c461673f512ea524067803d4a5c2208c2c0
Simon Zeni <simonzeni@gmail.com>
2019-05-24 14:18:32 -0400

    vector2 : untemplate operators & free function

--- dt/vector2.cpp
+++ dt/vector2.cpp
@@ -15,11 +15,28 @@ Vector2::dist2(const Vector2 &v) const
 double
 Vector2::dist(const Vector2 &v) const
 {
-	return hypot(x - v.x, y - v.y);}
+	return hypot(x - v.x, y - v.y);
 }
 
 double
 Vector2::norm2() const
 {
 	return x * x + y * y;
+}
+
+std::ostream&
+Vector2::operator <<(std::ostream &str)
+{
+	return str << "Point x: " << this->x << " y: " << this->y;
+}
+
+bool
+Vector2::operator ==(const Vector2 &v)
+{
+	return (this->x == v.x) && (this->y == v.y);
+}
+
+bool almost_equal(const Vector2 &v1, const Vector2 &v2, int ulp)
+{
+	return almost_equal(v1.x, v2.x, ulp) && almost_equal(v1.y, v2.y, ulp);
 }
--- include/vector2.h
+++ include/vector2.h
@@ -17,27 +17,15 @@ struct Vector2
 	double dist(const Vector2 &v) const;
 	double norm2() const;
 
+	Vector2 &operator=(const Vector2&) = default;
+	Vector2 &operator=(Vector2&&) = default;
+	bool operator ==(const Vector2 &v);
+	std::ostream &operator <<(std::ostream &str);
+
 	double x;
 	double y;
 };
 
-template<typename T>
-std::ostream &operator << (std::ostream &str, Vector2<T> const &point)
-{
-	return str << "Point x: " << point.x << " y: " << point.y;
-}
-
-template<typename T>
-bool operator == (const Vector2<T>& v1, const Vector2<T>& v2)
-{
-	return (v1.x == v2.x) && (v1.y == v2.y);
-}
-
-template<class T>
-typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
-    almost_equal(const Vector2<T>& v1, const Vector2<T>& v2, int ulp=2)
-{
-	return almost_equal(v1.x, v2.x, ulp) && almost_equal(v1.y, v2.y, ulp);
-}
+bool almost_equal(const Vector2 &v1, const Vector2 &v2, int ulp=2);
 
 #endif
